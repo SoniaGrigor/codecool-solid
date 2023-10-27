ab4687a0145fe9638fc2c549ecc3aebb4993fd95
Sonia Grigor <44436009+SoniaGrigor@users.noreply.github.com>
2023-10-27 22:56:41 +0300

    Final correct message (#4)
    Final correct message

---
+++ src/main/java/org/example/p1_single_responsability/good/WindowsConsole.java
@@ -0,0 +1,8 @@
+package org.example.p1_single_responsability.good;
+
+
+public class WindowsConsole implements Device {
+    public void display(String text){
+        System.out.println(text);
+    }
+}
---
+++ src/main/java/org/example/p2_open_closed/good/XMLReporting.java
@@ -0,0 +1,8 @@
+package org.example.p2_open_closed.good;
+
+public class XMLReporting implements Reporting {
+    @Override
+    public void generateReport() {
+        System.out.println("Generate XML Report");
+    }
+}
